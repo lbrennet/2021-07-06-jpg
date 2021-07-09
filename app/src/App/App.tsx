c866cd2734fee61484b343178e472d9dc3540233
lbrennet <laurentbrennetot@yahoo.fr>
2021-07-09 08:20:06 +0100

    add notes

--- app/src/App/App.tsx
+++ app/src/App/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <h1>Voici mon button</h1>
       <Button text="Une valeur" backColor="cyan"/>
       <Button text="Une autre valeur" backColor="tomato"/>
-      <Button text="Une autre autre valeur" />
+      <Button text="Une autre autre valeur" backColor="green"/>
     </div>
   );
 }
