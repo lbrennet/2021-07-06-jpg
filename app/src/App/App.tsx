56888fe8d4bddfa91e962afe04cb1fcae09f01d5
lbrennet <laurentbrennetot@yahoo.fr>
2021-07-09 10:40:51 +0100

    add react components

--- app/src/App/App.tsx
+++ app/src/App/App.tsx
@@ -1,12 +1,29 @@
 import React from 'react'
+import { Values } from './components/Values/Values';
 
 interface Props { }
+interface State {
+  values: Array<number>
+}
 
 export default class App extends React.Component<Props, {}> {
+  state: State
   constructor(props: Props) {
     super(props);
+    this.state = { values: [15, 12, 80, 7, 70] }
   }
   render() {
-    return (null);
+    return <div className="App">
+      <Values values={this.state.values}
+        addValue={(index: number) => {
+          console.log('add value');
+        }}
+        deleteValue={(index: number) => {
+          console.log('delete value')
+        }}
+      />
+      <hr />
+      {JSON.stringify(this.state)}
+    </div>
   }
 }
---
+++ app/src/App/components/Values/Values.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Button from '../Button/Button';
+
+interface Props {
+  values: Array<number>
+  addValue: Function
+  deleteValue: Function
+}
+
+export const Values: React.FunctionComponent<Props> = props => {
+  return <>  {/* balise vide react utilisee pour wrapper tout le flux en dessous dans une seule balise */}
+    <div>
+      <form onSubmit={(evt) => {
+        console.log(evt)
+        props.addValue(0)
+      }}>
+        <input type="number" />
+        <button type="submit">Ajouter valeur</button>
+      </form>
+    </div>
+    <div>
+      <h2>Resultats</h2>
+      <table>
+        <thead>
+          <tr>
+            <td>Value</td>
+            <td>actions</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            props.values.map((value, index, array) => {
+              return <tr><td>{value}</td><td>delete</td></tr>
+            })
+          }         
+        </tbody>
+      </table>
+    </div>
+  </>
+};
+
